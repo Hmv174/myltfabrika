003410f68db55ebcc9842d6b95558315a19c9b0e
Hmv174 <143493831+Hmv174@users.noreply.github.com>
2025-04-26 03:09:53 +0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "50px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "48px", color: "#333" }}>👋 Добро пожаловать в Multifabrika</h1>
+      <p style={{ fontSize: "20px", color: "#666" }}>
+        Здесь будет генерироваться мультсценарий на основе твоих героев и стиля!
+      </p>
+    </div>
+  );
+}
